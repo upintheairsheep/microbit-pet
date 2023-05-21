0da41543109e1da869bd6de65dc4c5b89d7fa531
upintheairsheep <43690204+upintheairsheep@users.noreply.github.com>
2023-05-20 20:45:55 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,4 +6,19 @@ basic.forever(function () {
     if (state == 0) {
         basic.showIcon(IconNames.Happy)
     }
+    if (state == 1) {
+        basic.showIcon(IconNames.Asleep)
+    }
+    if (state == 2) {
+        basic.showIcon(IconNames.Sad)
+    }
+    if (state == 3) {
+        basic.showIcon(IconNames.Confused)
+    }
+    if (state == 3) {
+        basic.showIcon(IconNames.Angry)
+    }
+    if (state == 4) {
+        basic.showIcon(IconNames.Ghost)
+    }
 })
